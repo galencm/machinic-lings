3f0006b716c1807ddf884f2fc9ddc3f2f2ff78ff
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-11 08:05:57 +0000

    Problem: imports change in package
    Solution: use 'from lings import' and update header
    license

--- lings/routes_remove.py
+++ lings/routes_remove.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-### This is a generated file ###
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import argparse
-import routeling
+from lings import routeling
 import sys
 import os
 def main(argv):
